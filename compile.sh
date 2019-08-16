1a1d164e530d8c3f2e695f5bacd112ce0a171e48
Jackson-S <Jackson-S@users.noreply.github.com>
2019-08-16 16:41:47 +1000

    Use xmllint to remove spaces from output file, reducing final compiled size.

--- compile.sh
+++ compile.sh
@@ -6,38 +6,56 @@ echo "Requires: Python3 and Jinja2 library (pip3 install jinja2)"
 
 echo "Setting up build directory"
 
+# Remove old directories if they exist
 rm -rf build
 rm -rf output
 
+# Create the output directory for the python script objects
+mkdir output
+
+# Create the output build directory structure
 mkdir build
+mkdir build/OtherResources
+mkdir build/OtherResources/Images
+
+# Copy assets into the correct place
 cp ./assets/Makefile ./build/Makefile
 cp ./assets/info.plist ./build/JapaneseDictionary.plist
 cp ./assets/style.css ./build/JapaneseDictionary.css
-mkdir build/OtherResources
 cp ./assets/prefs.html ./build/OtherResources/JapaneseDictionary_prefs.html
-mkdir build/OtherResources/Images
+
+# Extract the KanjiVG svg files to the output location
 tar -xzf ./assets/kanjivg.tar.xz -C ./build/OtherResources/Images
 
-mkdir output
-
+# Convert the sample sentences into a new, simplified XML file containing only needed data
 echo "Processing sample sentences"
 python3 ./sentence_converter.py ./input/sentences.csv ./input/jpn_indices.csv -o output/sentences.xml
+
+# Convert Kanjidic2.xml into a simplified XML file containing only the needed data
 echo "Processing Kanji"
 python3 ./kanjidic_converter.py ./input/kanjidic2.xml
+
+# Convert JMDict_e.xml into a simplified XML file containing only the needed data
 echo "Processing Dictionary"
 python3 ./dictionary_converter.py ./input/JMdict_e.xml
 
+# Combine the simplified XML files into the output Apple Dictionary XML file.
 echo "Combining processed files"
 python3 ./combiner.py ./output/dictionary.xml ./output/kanji.xml ./output/sentences.xml ./input/english.txt -o ./build/JapaneseDictionary.xml
 
+# Traverse to the output directory in preparation to build
 echo "Building dictionary (This will take a long time, i.e. 10+ minutes"
 echo "for unoptimised, 1-2 hours for optimised)"
 cd build
 
+# Compress the dictionary down, removing all spaces in the file (Reduces final compiled size)
+xmllint JapaneseDictionary.xml --noblanks > compressed.xml
+mv compressed.xml JapaneseDictionary.xml
+
 make
 make install
 
+# Return to initial directory and clean up build files
 cd ..
-
 rm -rf output
 rm -rf build
