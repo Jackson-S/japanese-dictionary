eb2c9d73cb7dbe04255fdd5167ef68d4f23a005a
Jackson-S <Jackson-S@users.noreply.github.com>
2019-10-02 11:51:36 +1000

    Remove unused code

--- classes/entry.py
+++
@@ -1,4 +0,0 @@
-class Entry:
-    def __init__(self, page_title: str, language: str, entry_type: str):
-        self.page_title: str = page_title
-        self.page_id: str = f"{language}_{entry_type}_{page_title}"
